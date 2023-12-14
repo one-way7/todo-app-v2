1fa7ce58842bd5bba6197d201053503fc3a34003
Vladislav <vladislav.work98@gmail.com>
2023-12-14 19:00:01 +0200

    added an event handler for displaying and hiding the project modal

--- src/handlers.js
+++ src/handlers.js
@@ -1,3 +1,22 @@
-const handlers = (() => {})();
+import dom from './dom';
+
+const handlers = (() => {
+  const clickHandler = () => {
+    dom.body.addEventListener('click', (e) => {
+      if (
+        e.target.classList.contains('add-project__icon') ||
+        e.target.parentNode.classList.contains('add-project__icon')
+      ) {
+        dom.showProjectModal();
+      } else if (e.target.classList.contains('project__form-cancel-btn')) {
+        dom.hideProjectModal();
+      }
+    });
+  };
+
+  return {
+    clickHandler,
+  };
+})();
 
 export default handlers;
