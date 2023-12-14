0b9c76a771f74652153628f31d00d19c3f329341
Vladislav <vladislav.work98@gmail.com>
2023-12-14 15:45:44 +0200

    added class Project and createProject function to create new projects

--- src/projects.js
+++ src/projects.js
@@ -1,3 +1,29 @@
-const projects = (() => {})();
+const projects = (() => {
+  const projectsList = [
+    {
+      title: 'Default',
+      tasks: [],
+    },
+  ];
+
+  class Project {
+    tasks = [];
+
+    constructor(title) {
+      this.title = title;
+    }
+  }
+
+  const createProject = (title) => {
+    const newProject = new Project(title);
+
+    projectsList.push(newProject);
+  };
+
+  return {
+    projectsList,
+    createProject,
+  };
+})();
 
 export default projects;
