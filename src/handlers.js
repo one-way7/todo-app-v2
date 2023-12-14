0f4bcc029cb79fa255ca4f2483d8a666d49291c0
Vladislav <vladislav.work98@gmail.com>
2023-12-14 21:55:02 +0200

    added the event handler on edit project icon and edit project button

--- src/handlers.js
+++ src/handlers.js
@@ -2,17 +2,39 @@ import dom from './dom';
 import validation from './validation';
 
 const handlers = (() => {
+  let projectIndex = 0;
+
   const clickHandler = () => {
     dom.body.addEventListener('click', (e) => {
       if (
         e.target.classList.contains('add-project__icon') ||
         e.target.parentNode.classList.contains('add-project__icon')
       ) {
+        dom.hideElement(dom.formProjectTitleError);
+        dom.hideEditProjectForm();
         dom.showProjectModal();
       } else if (e.target.classList.contains('project__form-cancel-btn')) {
-        dom.hideProjectModal();
+        if (
+          e.target.previousSibling.classList.contains('project__form-add-btn')
+        ) {
+          dom.hideProjectModal();
+        } else {
+          dom.hideEditProjectForm();
+        }
       } else if (e.target.classList.contains('project__form-add-btn')) {
         validation.addProject(e);
+      } else if (
+        e.target.classList.contains('project__edit-icon') ||
+        e.target.parentNode.classList.contains('project__edit-icon')
+      ) {
+        projectIndex = e.target
+          .closest('.project__item')
+          .getAttribute('data-index');
+        dom.hideElement(dom.formProjectTitleError);
+        dom.hideProjectModal();
+        dom.showEditProjectForm(projectIndex);
+      } else if (e.target.classList.contains('project__form-edit-btn')) {
+        validation.editProject(e, projectIndex);
       }
     });
   };
