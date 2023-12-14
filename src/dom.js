430920cdb1da4783d93cf9f51adf60b48aa4f51f
Vladislav <vladislav.work98@gmail.com>
2023-12-14 17:55:59 +0200

    added a function to render project form. Added conditions for rendering the edit form

--- src/dom.js
+++ src/dom.js
@@ -2,62 +2,15 @@ import projects from './projects';
 
 const dom = (() => {
   const projectsList = document.querySelector('.project__container');
+  const projectModal = document.querySelector('.project__modal');
 
-  const renderProjects = () => {
-    // create projectDiv
-    projectsList.textContent = '';
-
-    for (let i = 0; i < projects.projectsList.length; i += 1) {
-      const projectDiv = document.createElement('div');
-      projectDiv.classList.add('project__item');
-      projectDiv.setAttribute('data-index', i);
-      projectsList.appendChild(projectDiv);
-
-      // create content container
-      const projectContent = document.createElement('div');
-      projectContent.classList.add('project__content');
-      projectDiv.appendChild(projectContent);
-
-      // create icon
-      const projectIconWrapper = document.createElement('div');
-      projectIconWrapper.classList.add('project__icon');
-      projectContent.appendChild(projectIconWrapper);
-      const projectIcon = document.createElement('i');
-      projectIcon.classList.add('ri-terminal-line');
-      projectIconWrapper.appendChild(projectIcon);
-
-      // create title
-      const projectTitle = document.createElement('p');
-      projectTitle.textContent = projects.projectsList[i].title;
-      projectTitle.classList.add('project__title');
-      projectContent.appendChild(projectTitle);
-
-      // create settings icon
-      const projectIconsContainer = document.createElement('div');
-      projectIconsContainer.classList.add('project__setting-icons');
-      projectDiv.appendChild(projectIconsContainer);
-
-      const editIconWrapper = document.createElement('div');
-      editIconWrapper.classList.add('project__edit-icon');
-      projectIconsContainer.appendChild(editIconWrapper);
-
-      const editIcon = document.createElement('i');
-      editIcon.classList.add('ri-edit-line');
-      editIconWrapper.appendChild(editIcon);
-
-      const deleteIconWrapper = document.createElement('div');
-      deleteIconWrapper.classList.add('project__delete-icon');
-      projectIconsContainer.appendChild(deleteIconWrapper);
-
-      const deleteIcon = document.createElement('i');
-      deleteIcon.classList.add('ri-close-line');
-      deleteIconWrapper.appendChild(deleteIcon);
-    }
-
-    // add new form line
+  const renderFormProjects = (modal = 'add') => {
+    const addBtn = modal === 'add' ? 'add' : 'Edit';
+    const isHide = modal === 'add';
 
     const projectFormDiv = document.createElement('div');
-    projectFormDiv.classList.add('project__modal', 'hide');
+    projectFormDiv.classList.add('project__modal');
+    if (isHide) projectFormDiv.classList.add('hide');
     projectsList.appendChild(projectFormDiv);
 
     const projectForm = document.createElement('form');
@@ -91,7 +44,7 @@ const dom = (() => {
     const formAddBtn = document.createElement('button');
     formAddBtn.classList.add('project__form-add-btn');
     formAddBtn.setAttribute('type', 'submit');
-    formAddBtn.textContent = 'Add';
+    formAddBtn.textContent = addBtn;
     formBtnsWrapper.appendChild(formAddBtn);
 
     const formCancelBtn = document.createElement('button');
@@ -99,6 +52,66 @@ const dom = (() => {
     formCancelBtn.setAttribute('type', 'button');
     formCancelBtn.textContent = 'Cancel';
     formBtnsWrapper.appendChild(formCancelBtn);
+  };
+
+  const renderProjects = (projectIndex) => {
+    // create projectDiv
+    projectsList.textContent = '';
+
+    for (let i = 0; i < projects.projectsList.length; i += 1) {
+      const projectDiv = document.createElement('div');
+
+      if (parseInt(projectIndex, 10) !== i) {
+        projectDiv.classList.add('project__item');
+        projectDiv.setAttribute('data-index', i);
+        projectsList.appendChild(projectDiv);
+
+        // create content container
+        const projectContent = document.createElement('div');
+        projectContent.classList.add('project__content');
+        projectDiv.appendChild(projectContent);
+
+        // create icon
+        const projectIconWrapper = document.createElement('div');
+        projectIconWrapper.classList.add('project__icon');
+        projectContent.appendChild(projectIconWrapper);
+        const projectIcon = document.createElement('i');
+        projectIcon.classList.add('ri-terminal-line');
+        projectIconWrapper.appendChild(projectIcon);
+
+        // create title
+        const projectTitle = document.createElement('p');
+        projectTitle.textContent = projects.projectsList[i].title;
+        projectTitle.classList.add('project__title');
+        projectContent.appendChild(projectTitle);
+
+        // create settings icon
+        const projectIconsContainer = document.createElement('div');
+        projectIconsContainer.classList.add('project__setting-icons');
+        projectDiv.appendChild(projectIconsContainer);
+
+        const editIconWrapper = document.createElement('div');
+        editIconWrapper.classList.add('project__edit-icon');
+        projectIconsContainer.appendChild(editIconWrapper);
+
+        const editIcon = document.createElement('i');
+        editIcon.classList.add('ri-edit-line');
+        editIconWrapper.appendChild(editIcon);
+
+        const deleteIconWrapper = document.createElement('div');
+        deleteIconWrapper.classList.add('project__delete-icon');
+        projectIconsContainer.appendChild(deleteIconWrapper);
+
+        const deleteIcon = document.createElement('i');
+        deleteIcon.classList.add('ri-close-line');
+        deleteIconWrapper.appendChild(deleteIcon);
+      } else {
+        renderFormProjects('edit');
+      }
+    }
+
+    // add new form line
+    renderFormProjects();
   };
 
   return {
