0d8794e2413fc778c0f83ec2ad8ce51ab918a268
Vladislav <vladislav.work98@gmail.com>
2023-12-15 09:54:44 +0200

    added an event handler when clicking on a link

--- src/handlers.js
+++ src/handlers.js
@@ -4,6 +4,7 @@ import projects from './projects';
 
 const handlers = (() => {
   let projectIndex = 0;
+  let link = 'inbox';
 
   const clickHandler = () => {
     dom.body.addEventListener('click', (e) => {
@@ -28,9 +29,10 @@ const handlers = (() => {
         e.target.classList.contains('project__edit-icon') ||
         e.target.parentNode.classList.contains('project__edit-icon')
       ) {
-        projectIndex = e.target
-          .closest('.project__item')
-          .getAttribute('data-index');
+        projectIndex = parseInt(
+          e.target.closest('.project__item').getAttribute('data-index'),
+          10,
+        );
         dom.hideElement(dom.formProjectTitleError);
         dom.hideProjectModal();
         dom.showEditProjectForm(projectIndex);
@@ -40,10 +42,45 @@ const handlers = (() => {
         e.target.classList.contains('project__delete-icon') ||
         e.target.parentNode.classList.contains('project__delete-icon')
       ) {
-        projectIndex = e.target
-          .closest('.project__item')
-          .getAttribute('data-index');
+        projectIndex = parseInt(
+          e.target.closest('.project__item').getAttribute('data-index'),
+          10,
+        );
         projects.removeProject(projectIndex);
+      } else if (
+        e.target.classList.contains('project__item') ||
+        e.target.closest('.project__item')
+      ) {
+        projectIndex = parseInt(
+          e.target.closest('.project__item').getAttribute('data-index'),
+          10,
+        );
+        dom.changeLink(projectIndex);
+      } else if (
+        e.target.closest('.nav__link').classList.contains('inbox__link')
+      ) {
+        link = 'inbox';
+        dom.changeLink('inbox');
+      } else if (
+        e.target.closest('.nav__link').classList.contains('today__link')
+      ) {
+        link = 'today';
+        dom.changeLink('today');
+      } else if (
+        e.target.closest('.nav__link').classList.contains('next__link')
+      ) {
+        link = 'week';
+        dom.changeLink('week');
+      } else if (
+        e.target.closest('.nav__link').classList.contains('important__link')
+      ) {
+        link = 'important';
+        dom.changeLink('important');
+      } else if (
+        e.target.closest('.nav__link').classList.contains('completed__link')
+      ) {
+        link = 'completed';
+        dom.changeLink('completed');
       }
     });
   };
