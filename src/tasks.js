286e65e90b84aa6b8f16e32c064f9a60f4fd8e99
Vladislav <vladislav.work98@gmail.com>
2023-12-15 12:28:49 +0200

    added a function to create new task

--- src/tasks.js
+++ src/tasks.js
@@ -1,1 +1,29 @@
-const tasks = (() => {})();
+import projects from './projects';
+import dom from './dom';
+
+const tasks = (() => {
+  class Task {
+    important = false;
+
+    done = false;
+
+    constructor(title, date) {
+      this.title = title;
+      this.date = date;
+    }
+  }
+
+  const createTask = (projectIndex, title, date = '', link = projectIndex) => {
+    const newTask = new Task(title, date);
+
+    projects.projectsList[projectIndex].tasks.push(newTask);
+
+    dom.renderTasks(projectIndex);
+  };
+
+  return {
+    createTask,
+  };
+})();
+
+export default tasks;
