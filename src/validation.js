fbb02a9e23ede2e40e4df7f35fb23293fb9d19a8
Vladislav <vladislav.work98@gmail.com>
2023-12-15 20:51:36 +0200

    added edit task function

--- src/validation.js
+++ src/validation.js
@@ -3,14 +3,14 @@ import projects from './projects';
 import tasks from './tasks';
 
 const validation = (() => {
-  const addProject = (e) => {
+  const addProject = (e, projectIndex, link) => {
     const projectTitle = document.forms['project__form']['title'].value;
 
     e.preventDefault();
 
     if (projectTitle !== '') {
       dom.hideProjectModal();
-      projects.createProject(projectTitle);
+      projects.createProject(projectTitle, projectIndex, link);
       dom.hideElement(dom.formProjectTitleError);
     } else if (projectTitle === '') {
       dom.showElement(dom.formProjectTitleError);
@@ -38,18 +38,31 @@ const validation = (() => {
 
     if (taskTitle !== '') {
       dom.hideElement(dom.formTaskTitleError);
+      dom.hideAddTaskForm();
       tasks.createTask(projectIndex, taskTitle, taskDate);
     } else {
       dom.showElement(dom.formTaskTitleError);
     }
   };
 
-  const editTask = (e, projectIndex, taskIndex) => {};
+  const editTask = (e, projectIndex, taskIndex, link) => {
+    e.preventDefault();
+    const taskTitle = document.forms['task__edit-form']['title'].value;
+    const taskDate = document.forms['task__edit-form']['date'].value;
+
+    if (taskTitle !== '') {
+      dom.hideElement(dom.formTaskTitleError);
+      tasks.editTask(taskTitle, taskDate, projectIndex, taskIndex, link);
+    } else {
+      dom.showElement(dom.formTaskTitleError);
+    }
+  };
 
   return {
     addProject,
     editProject,
     addTask,
+    editTask,
   };
 })();
 
